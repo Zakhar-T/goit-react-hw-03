bf93ee4f1a875bc8d04535e60a7f74c54c0c42e0
Zakhar-T <zakhar.null@gmail.com>
2025-05-02 23:07:41 +0300

    final edit

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { nanoid } from 'nanoid';
 import ContactList from '../ContactList/ContactList';
 import css from './App.module.css';
@@ -6,21 +6,28 @@ import SearchBox from '../SearchBox/SearchBox';
 import ContactForm from '../ContactForm/ContactForm';
 
 export default function App() {
-  const [contacts, setContacts] = useState(
-    JSON.parse(localStorage.getItem('savedContacts')) || [
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = JSON.parse(localStorage.getItem('savedContacts'));
+    if (savedContacts !== null) {
+      return savedContacts;
+    }
+    return [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
-  );
+    ];
+  });
   const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    window.localStorage.setItem('savedContacts', JSON.stringify(contacts));
+  }, [contacts]);
 
   const createContact = (name, number, actions) => {
     setContacts(() => {
       return [...contacts, { id: nanoid(), name: name, number: number }];
     });
-    localStorage.setItem('savedContacts', JSON.stringify(contacts));
     actions.resetForm();
   };
 
